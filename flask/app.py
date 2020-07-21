c7e56d3335fbc83308363880540db2c1181c330c
Rossy167 <37879439+Rossy167@users.noreply.github.com>
2020-07-21 22:20:26 +0100

    Getting started with Flask

---
+++ flask/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "woah!"
+
+@app.route("/yeet")
+def yeet():
+    return "yeet"
+
+page_views = 0
+
+@app.route("/views")
+def views():
+    global page_views
+    page_views += 1
+    return str(page_views)
+
+@app.route("/date")
+def date():
+    return render_template("index.html", user_image = 'https://i.pinimg.com/236x/ff/8a/1d/ff8a1d99f12501dbe68b3aac94690d2e.jpg')
